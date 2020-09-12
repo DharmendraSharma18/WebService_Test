6f92ce63033872b9983a8f7fe8a0228b57c46a04
dharam <vicky.253150@gmail.com>
2020-09-12 20:53:17 +0530

    added lib file and report, log, output modified

---
+++ Lib/RobotFrameworkInstallation.bat
@@ -0,0 +1,8 @@
+pip install jsonpath
+pip install robotframework
+pip install robotframework-SeleniumLibrary
+pip install robotframework-jsonlibrary
+pip install wxPython
+pip install robotframework-ride
+pip install Collections
+pip install Requests
